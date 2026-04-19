977366b76e4a11346bccfaeb7a380d644fec1258
CodeRonin <golla.programmazione@gmail.com>
2026-04-19 17:37:49 +0200

    feat: Create GameBoard component with TypeScript

---
+++ GameBoard.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import styles from './GameBoard.module.css';
+import { initializeGameBoard } from './GameBoard.logic';
+
+interface GameBoardProps {
+  width?: number;
+  height?: number;
+}
+
+const GameBoard: React.FC<GameBoardProps> = ({ width = 800, height = 600 }) => {
+  const [config, setConfig] = useState<any>(null);
+
+  useEffect(() => {
+    const boardConfig = initializeGameBoard({ width, height });
+    setConfig(boardConfig);
+  }, [width, height]);
+
+  return (
+    <div className={styles.gameBoardContainer}>
+      <div className={styles.gameBoard} style={{ width, height }}>
+        <div className={styles.felt}>
+          <div className={styles.centerSpot} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GameBoard;
